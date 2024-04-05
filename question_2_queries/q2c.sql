bd390b8b282a9318b838e6fe7b77df5f4f4007a1
Eduardo Vasquez-Villalpando <evasquez1192@gmail.com>
2024-04-05 10:39:21 -0500

    Update q2c.sql
    Changed to order by strike rate, rather than batter runs. Added having clause to filter guys with small amounts of data

--- question_2_queries/q2c.sql
+++ question_2_queries/q2c.sql
@@ -1,3 +1,6 @@
+select 
+  * 
+from (
 select
   batter,
   (sum(batter_runs)*100.0) / (count(*)-sum(case when wides>0 then 1 else 0 end)) as strike_rate
@@ -7,7 +10,7 @@ where match_year = 2023
 group by
   batter
 having
-  sum(batter_runs) > 10
-order by
-  sum(batter_runs) desc
+  sum(batter_runs) > 50
+)
+  order by strike_rate desc
 limit 5;
